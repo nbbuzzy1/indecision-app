e5c400103709e67352ab7a4eee064e3fc88e64e0
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2018-10-28 08:29:58 -0400

    changed modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -10,7 +10,7 @@ const OptionModal = (props) => (
     closeTimeoutMS={200}
     className="modal"
   >
-    <h3 className="modal__title">Selected Option</h3>
+    <h3 className="modal__title">You Should</h3>
     {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
     <button className="button" onClick={props.handleClearSelectedOption}>Okay</button>
   </Modal>
